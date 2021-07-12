99c15059936e614a058eda9ee189c995469fc430
Mr yuan <2205968704@qq.com>
2021-07-12 13:29:34 +0800

    服务注册 会员调用优惠卷服务

--- gulimall-member/src/main/java/com/atguigu/gulimall/member/feign/CouponFeiService.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/feign/CouponFeiService.java
@@ -1,13 +1,26 @@
 package com.atguigu.gulimall.member.feign;
 
+import com.atguigu.common.utils.R;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * 告诉spring 这个一个远程服务 调用远程客户端 gulimall-coupon 调用哪个写哪个
  */
 @FeignClient("gulimall-coupon")
 public interface CouponFeiService {
+
+
+    /**
+     * 完整路径+方法
+     * feign去服务注册中心找 gulimall-coupon 里面的 /coupon/coupon/member/list
+     * 大概就是这个意思
+     */
+
+    @RequestMapping("/coupon/coupon/member/list")
+    public R memberCoupons();
 }
+
 
 
 
